2cb08ec13bd9ce3058227839fed21ba05b971258
sanjaymusale <sanjaymusale.10@gmail.com>
2019-02-03 14:22:06 +0530

    Update temparature.js

--- day14-axios/temparature.js
+++ day14-axios/temparature.js
@@ -2,7 +2,7 @@ const axios=require('axios')
 let lat = process.argv[2]
 let lng=process.argv[3]
 
-let url2=`https://api.darksky.net/forecast/7d9e69b2c4bd31c94faf3b9f5c479028/${lat},${lng}`
+let url2=`https://api.darksky.net/forecast/ky/${lat},${lng}`
 
 axios.get(url2).then(function(response){
   let result = response.data
